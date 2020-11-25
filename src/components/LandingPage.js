1cf19b59ba73e82d69b5f665f017a287741ce12d
raph941 <raph941@gmail.com>
2020-11-25 21:34:18 +0100

    chore

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/Landing.css';
 import ChatImage from '../images/talk3.png';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
