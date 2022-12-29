b23b8615e0764f10d9eb7a677e1dd62b2ad20f4a
Raphael Ehindero <raph941@gmail.com>
2022-12-29 18:11:54 +0100

    Update axios.js

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -11,5 +11,5 @@ export default instance
 // export const baseURL = `${process.env.REACT_APP_API_PATH}`
 // export const SOCKET_PATH = `${process.env.REACT_APP_SOCKET_PATH}`
 
-export const baseURL = 'https://chatapp-be-api.herokuapp.com/'
-export const SOCKET_PATH = 'wss://chatapp-be-api.herokuapp.com/chat/';
+export const baseURL = 'https://chatapp-api-cc07.onrender.com/'
+export const SOCKET_PATH = 'wss://chatapp-api-cc07.onrender.com/chat/';
